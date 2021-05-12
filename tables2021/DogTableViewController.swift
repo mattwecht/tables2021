d6fb2a1daaa5497fd909219d14035922891259ae
Matt Wecht <mattwecht@gmail.com>
2021-05-12 11:59:46 -0400

    Iteration 1: Custom values

--- tables2021/DogTableViewController.swift
+++ tables2021/DogTableViewController.swift
@@ -7,75 +7,52 @@
 
 import UIKit
 
+struct Dogs {
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+}//this just contains properties, like an interface
+
 class DogTableViewController: UITableViewController {
+
+    var allDogs = [
+          Dogs(id: 1,
+                title: "Poodle",
+                text: "ITS SO FLUFFY",
+                image: "poodle-pup"),
+          Dogs(id: 2,
+                title: "German Shepard",
+                text: "Now thats a big floof",
+                image: "angry-fluff"),
+          Dogs(id: 3,
+                title: "Mutt",
+                text: "Where it came from? No one knows. Where it belongs? My heart",
+                image: "sammy")
+      ]
 
    
     override func numberOfSections(in tableView: UITableView) -> Int {
        
-        return 3//will only return 3
+        return 1//will only return 1 overall section of rows
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return 5
+        return allDogs.count//changes to the amount of dogs in my array
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)//replaced string with our own label from story board
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"//changes label to say the location
+        cell.textLabel?.text = allDogs[indexPath.row].title
 
         return cell
     }//function was uncomented
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "section \(section)"
+        return "Types of Dogs"
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
